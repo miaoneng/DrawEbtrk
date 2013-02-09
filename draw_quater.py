c16c887c2346b7639495a1c5837c024febc09742
striges <sugar1987cn@gmail.com>
2013-02-09 01:34:58 -0500

    1
    Signed-off-by: striges <sugar1987cn@gmail.com>

--- draw_quater.py
+++ draw_quater.py
@@ -4,35 +4,28 @@ Created on Jan 6, 2013
 @author: striges
 '''
 
-import arcpy
 import math
 
 class Quarter:
     """Draw a quarter"""
     
-    def __init__(self, NE=-99, SE=-99, SW=-99, NW=-99):
-        self.radius = (NE,SE,SW,NW)
+    def __init__(self, NE=-99, SE=-99, SW=-99, NW=-99, Center=(0,0), Fill_Missing=False):
+        self.radius = (NE,SE,SW,NW)          
+        self.X, self.Y = Center
         
     def __draw_quadrant(self,radius=-99,direction=0):
         """0 - 1st quadrant; 1 - 2nd quadrant; 2 - 3rd quadrant; 3 - 4th quadrant"""
         if radius < 0:
             return None
         ptlist = []
-        for a in range(90):
+        for a in range(91):
             arc = (direction * 90 + a) / 180.0 * math.pi
-            ptlist.append((radius * math.cos(arc), radius * math.sin(arc)))
+            ptlist.append([radius * math.cos(arc) + self.X, radius * math.sin(arc) + self.Y])
         return ptlist 
-    
-    def __draw_ellipse(self):
-        pass
-    
-    def get_geometry(self):
-        self.get_pointList()
-        g = arcpy.Polygon()
-        pass
-    
+       
     def get_pointList(self):
         self.pointList = []
-        for p in map(self.radius, range(4), self.__draw_quadrant):
+        for p in map(self.__draw_quadrant, self.radius, range(4)):
             self.pointList += p
+        self.pointList.append(self.pointList[0])
         return self.pointList
--- mainTest.py
+++ mainTest.py
@@ -15,7 +15,7 @@ class TestMain(unittest.TestCase):
     def tearDown(self):
         pass
 
-    def test_read(self):
+    def passed_test_read(self):
         import main
         self.Worker = main.Worker()
         self.wb = self.Worker.wb
@@ -28,8 +28,20 @@ class TestMain(unittest.TestCase):
         return True
     
     def test_quadrant(self):
-        from draw_utils import draw_quater
-        
+        import os
+        homedir = os.environ['USERPROFILE']
+        import draw_quater
+        d = draw_quater.Quarter(100,50,25,12.5,(0,0))
+        from pprint import pprint as pp        
+        pp(d.get_pointList())
+        import shapefile
+        w = shapefile.Writer(shapefile.POLYGON)
+        w.autoBalance = 1
+        w.poly(parts=[d.get_pointList()])
+        w.field('FIRST_FLD', 'C', '40')
+        w.record('3', 'Polygon')
+        w.save(homedir + '/test')
+        return True
 
 if __name__ == "__main__":
     #import sys;sys.argv = ['', 'Test.testName']
